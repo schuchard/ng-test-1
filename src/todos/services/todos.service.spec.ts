81264dfc7eaab25f58487b0ee461d7c702b3b049
Chris Kilmer <christopherkilmer@gmail.com>
2018-10-14 14:53:40 -0600

    Add specs for Todo service loading.

--- src/todos/services/todos.service.spec.ts
+++ src/todos/services/todos.service.spec.ts
@@ -1,12 +1,28 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
-import { TodosService } from './todos.service';
+import { TodosService } from '@todos/services/todos.service';
 
 describe('TodosService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  const todos = [
+    { id: 1, description: 'Todo #1', completed: false },
+    { id: 2, description: 'Todo #2', completed: true }
+  ];
 
-  it('should be created', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TodosService]
+    });
+
     const service: TodosService = TestBed.get(TodosService);
-    expect(service).toBeTruthy();
+    spyOn(service, 'getTodos').and.returnValue(todos);
+  });
+
+  describe('getTodos', () => {
+    it('should get all Todos', () => {
+      const service: TodosService = TestBed.get(TodosService);
+
+      expect(service.getTodos()).toEqual(todos);
+    });
   });
 });
